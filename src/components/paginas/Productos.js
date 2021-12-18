7f38d5c1c55f984142fb2290b00043e99e011f8d
CRISTIAN FABIAN GOMEZ <cristianf.gomez.mt@correo.usa.edu.co>
2021-12-18 11:53:17 -0500

    first commit

---
+++ src/components/paginas/Productos.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import FormularioProductos from "../ui/FormularioProductos";
+
+const Productos = () => {
+  const [productos, guardarProductos] = useState([]);
+
+  fetch("http://localhost:8080/api/cleaningprod/all")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      guardarProductos(data);
+    });
+  return (
+    <>
+      <h1 className="text-3xl font-light mb-4">Productos</h1>
+      <Link
+        to="/nuevo-producto"
+        className="  bg-blue-800 hover:bg-blue-700, inline-block mb-5 p-2 text-white uppercase font-bold"
+      >
+        Agregar Producto
+      </Link>
+      {productos.map((producto) => (
+        <FormularioProductos 
+        key={producto.id}
+        producto={producto}
+        />
+        
+      ))}
+    </>
+  );
+};
+
+export default Productos;
